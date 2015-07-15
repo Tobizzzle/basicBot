e6301541b5d931f274890a5305fe80d38a6e3f3d
Tobizzzle <Tobizzzle@users.noreply.github.com>
2015-07-15 10:54:40 -0700

    Update basicBot.js
    added fishing command (beta)

--- basicBot.js
+++ basicBot.js
@@ -3092,7 +3092,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me @ caught a fish ".");
+                        API.sendChat('/me Someone wants to go fishing!');
                     }
                 }
             },
