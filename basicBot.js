59075e0747eb6d6cb6eabd845b53c30dc856570a
Tobizzzle <Tobizzzle@users.noreply.github.com>
2015-07-15 11:17:19 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3092,7 +3092,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me You just caught a Goldfish!');
+                        API.sendChat('/me @ You just caught a Goldfish!');
                     }
                 }
             },
