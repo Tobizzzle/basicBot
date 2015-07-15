e6d2cb0860065f8b537aa3a04450d26fee2de026
Tobizzzle <Tobizzzle@users.noreply.github.com>
2015-07-15 11:15:31 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3084,15 +3084,15 @@
                     }
                 }
             },
-            fishhelpCommand: {
-                command: 'fish help',
+            fishCommand: {
+                command: 'fish',
                 rank: 'user',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me The commands for the fishing game are not yet ready ".");
+                        API.sendChat('/me @ caught a Goldfish');
                     }
                 }
             },
