857eb80739b5269505e1321fbe80332667bd688a
Tobizzzle <Tobizzzle@users.noreply.github.com>
2015-08-03 19:34:44 -0700

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3084,6 +3084,18 @@
                     }
                 }
             },
+                        ripCommand: {
+                command: 'rip',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat('/me Resteroni in pepperoni.');
+                    }
+                }
+            },
             fishCommand: {
                 command: 'fish',
                 rank: 'user',
